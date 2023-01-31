07623460f87e4792acbefdfc83df0404f97c9dbe
comp <vitaliivorob3@gmail.com>
2023-01-31 20:04:02 +0300

    Hw-13-3

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,19 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.BiConsumer;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.Arrays.stream;
-import static jdk.vm.ci.sparc.SPARC.o1;
-import static jdk.vm.ci.sparc.SPARC.o2;
 
 
 public class Main {
 
+
+    private static Integer t2;
 
     public static void main(String[] args) {
 
@@ -47,21 +48,15 @@ public class Main {
 
 
 
-    private static int order (Integer o2, Integer o1){
-        if (o1 > o2) {
-            return 1;
-        } else if (o2 > o1) {
-            return -1;
-        }
-        return 0;
-    }
+
 
     public static <T, U> BiConsumer<T, U>  findMinMax
             (BiConsumer<? super T, ? super T> minMaxConsumer,
              Comparator<? super T> order,
              Stream<? extends T> stream){
-        T max = stream(stream.toArray()).max(order(T o2, T o1)).get();
-        T min = stream(stream.toArray()).min(order(T o2, T o1)).get();
+
+        T max = stream(stream.toArray()).max(order).get();
+        T min = stream(stream.toArray()).min(order).get();
         return minMaxConsumer.accept(min, max);
 
     }
